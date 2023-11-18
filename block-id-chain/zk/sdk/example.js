b429a821f759da58d1b1b909506ce1f7abb410fe
toprakkeskin <toprak.keskn@gmail.com>
2023-11-18 20:23:47 +0300

    Complete zk sdk verifier and example

--- block-id-chain/zk/sdk/example.js
+++ block-id-chain/zk/sdk/example.js
@@ -14,21 +14,37 @@ const outputPublicSignalsFilePath = circuitPath + "BasicIdentity_js/BasicIdentit
 
 async function runExample() {
   // Prepare input for witness generation.
+  console.log("Preparing input for witness generation...");
   const input = {
     client_id_and_full_name: hashSignals("client_id_1", "toprak keskin"),
     identity_number_and_nationality: hashSignals("12345678910", "TC"),
     date_of_birth_padded: hashSignals("05.06.1995", "0"),
   };
+
   // Generate witness.
+  console.log("Generating witness...");
   await prover.generateWitness(input, circuitWasmFilePath, outputWitnessFilePath);
+
   // Generate proof and public signals.
+  console.log("Generating proof and public signals...");
   const { proof, publicSignals } = await prover.prove(outputWitnessFilePath, zkeyFilePath, outputProofFilePath, outputPublicSignalsFilePath);
   // Save proof and public signals to files.
   fs.writeFileSync(outputProofFilePath, JSON.stringify(proof, null, 1));
   fs.writeFileSync(outputPublicSignalsFilePath, JSON.stringify(publicSignals, null, 1));
 
-
-  verifier.verify();
+  // Verify proof.
+  console.log("Verifying proof...")
+  const vkey = JSON.parse(fs.readFileSync(vkeyFilePath).toString());
+  const verified = verifier.verify(vkey, publicSignals, proof);
+  if (!verified) {
+    throw new Error("Proof is not verified!");
+  }
+  console.log("DONE!")
 }
 
-runExample();
+runExample().then(() => {
+  process.exit(0);
+}).catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
--- block-id-chain/zk/sdk/verifier.js
+++ block-id-chain/zk/sdk/verifier.js
@@ -1,4 +1,11 @@
-exports.verify = function () {
-  // TODO implement
-  console.log("verified!")
+
+const snarkjs = require("snarkjs");
+
+exports.generateWitness = async function (input, circuitWasmFilePath, outputWitnessFilePath) {
+  await snarkjs.wtns.calculate(input, circuitWasmFilePath, outputWitnessFilePath);
+};
+
+exports.verify = async function (vkey, publicSignals, proof) {
+  return await snarkjs.groth16.verify(vkey, publicSignals, proof)
 }
+
