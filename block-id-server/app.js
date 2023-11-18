a2a661815a9d5a7fd6aad64f71c76104f82d43c5
Samed Kahyaoglu <32146055+urtuba@users.noreply.github.com>
2023-11-19 01:03:23 +0300

    Remove unused packages

--- block-id-server/app.js
+++ block-id-server/app.js
@@ -1,13 +1,11 @@
 // simple express server
 
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 4000;
 const app = express();
 
-app.use(bodyParser.json())
-app.get('/health', (req, res) => { res.send({ status: 'ok' }) })
+app.get('/health', (req, res) => { res.send('OK') })
 
 // INIT BLOCKHAIN LISTENER PROCESS THAT WILL BE ALIVE FOR THE LIFE OF THE SERVER
 
