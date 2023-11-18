d1761caa125a65100b03fbc1b00a97a691c96907
Samed Kahyaoglu <32146055+urtuba@users.noreply.github.com>
2023-11-18 03:38:53 +0300

    Init example client exchange

---
+++ client-server/index.js
@@ -0,0 +1,6 @@
+// express app
+// registers routes:
+//  - GET /api/health
+//  - GET /api/block-id/grant-code  - get grant code 
+//  - GET /api/block-id/proof       - get proof of identity
+//  - GET /api/block-id/info        - get identity information
