056fc504bffc70a2b1abbc6149e193b9d5ece5ad
Samed Kahyaoglu <32146055+urtuba@users.noreply.github.com>
2023-11-18 04:41:20 +0300

    Init app register endpoints

--- client-server/index.js
+++ client-server/index.js
@@ -1,6 +1,31 @@
 // express app
 // registers routes:
 //  - GET /api/health
-//  - GET /api/block-id/grant-code  - get grant code 
+//  - POST /api/block-id/grant-code  - get grant code 
 //  - GET /api/block-id/proof       - get proof of identity
 //  - GET /api/block-id/info        - get identity information
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const connectDb = require('./utils/db')
+const authorizeBlockId = require('./utils/authorize-block-id')
+const blockIdGrantCode = require('./routes/block-id-grant-code')
+const blockIdProof = require('./routes/block-id-proof')
+const blockIdInfo = require('./routes/block-id-info')
+
+connectDb()
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(bodyParser.json())
+
+app.use('/api/health', (req, res) => { res.send('OK')})
+app.post('/api/block-id/grant-code', authorizeBlockId, blockIdGrantCode)
+app.get('/api/block-id/proof', authorizeBlockId, blockIdProof)
+app.get('/api/block-id/exchange', blockIdInfo)
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
+
+
+
