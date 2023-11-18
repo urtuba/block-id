8e017f8c620cee04fdd1d7dba1e5af5a9c40a40a
Samed Kahyaoglu <32146055+urtuba@users.noreply.github.com>
2023-11-19 01:37:28 +0300

    Add data_hash to circuit to validate data integrity between clients

--- block-id-server/utils/zk.js
+++ block-id-server/utils/zk.js
@@ -3,3 +3,7 @@
 // 2. An util function 'validateIdentityProofs(...zkProofs)' will take an array of proofs:
 //    - Validate if all proofs are created based on same identity
 //    - Returns the result [true/false] and the identity [walletAddress]
+
+const validateDataConsistency = (zkProofs) => {
+  
+}
