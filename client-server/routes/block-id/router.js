33b9ab51fdf9b2045cdc45fd35329a431d3a39ac
Samed Kahyaoglu <32146055+urtuba@users.noreply.github.com>
2023-11-18 13:41:12 +0300

    Add block id callback

--- client-server/routes/block-id/router.js
+++ client-server/routes/block-id/router.js
@@ -3,8 +3,9 @@ const authorizeBlockId = require( '../../utils/authorize-block-id' )
 
 const blockIdRouter = express.Router()
 
-blockIdRouter.post('/grant-code', authorizeBlockId, require('./grant-code'))
 blockIdRouter.get('/proof', authorizeBlockId, require('./proof'))
 blockIdRouter.get('/exchange', require('./exchange'))
+blockIdRouter.post('/callback', require('./callback'))
+blockIdRouter.post('/grant-code', authorizeBlockId, require('./grant-code'))
 
 module.exports = blockIdRouter
