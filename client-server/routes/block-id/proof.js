ed10b64c48acec5fdc2cc4b049ce2d0d6c59bfdd
Samed Kahyaoglu <32146055+urtuba@users.noreply.github.com>
2023-11-18 23:26:15 +0300

    Update proof.js

--- client-server/routes/block-id/proof.js
+++ client-server/routes/block-id/proof.js
@@ -3,73 +3,109 @@ const path = require("path");
 const { prover, utils } = require("block-id-sdk");
 const { hashSignals } = utils;
 
+const User = require('../../models/user.js')
+const IdentityVerification = require('../../models/identity-verification.js')
+
 const appDir = path.dirname(require.main.filename);
 const circuitPath = utils.getArtifactsPath() + "/circuits/CompleteIDVerification/";
 const circuitWasmFilePath = circuitPath + "CompleteIDVerification_js/CompleteIDVerification.wasm";
 const zkeyFilePath = circuitPath + "CompleteIDVerification.zkey";
 // const vkeyFilePath = circuitPath + "CompleteIDVerification.vkey";
 
-
 /**
  * @swagger
- * /block-id/proof:
+ * /api/identity-verification:
  *   get:
- *     summary: Retrieve a proof from Block ID.
+ *     summary: Retrieve identity verification proof and public signals.
  *     tags:
- *       - Block ID
+ *       - Identity Verification
  *     parameters:
- *       - in: header
- *         name: block-id-api-key
+ *       - in: query
+ *         name: wallet
  *         required: true
+ *         description: Wallet address of the user to verify.
  *         schema:
  *           type: string
- *         description: API key needed to authorize the request.
+ *       - in: query
+ *         name: block-id-api-key
+ *         required: true
+ *         description: Authenticates block id requests
+ *         schema:
+ *           type: string
  *     responses:
  *       200:
- *         description: Proof retrieved successfully.
+ *         description: Successfully retrieved the identity verification proof and public signals.
  *         content:
  *           application/json:
  *             schema:
  *               type: object
  *               properties:
  *                 proof:
- *                   type: string
- *                   description: The proof from Block ID.
+ *                   type: object
+ *                   description: The proof of identity verification.
+ *                 publicSignals:
+ *                   type: array
+ *                   items:
+ *                     type: string
+ *                   description: Public signals associated with the proof.
+ *       400:
+ *         description: Bad request. Wallet parameter is missing.
+ *       404:
+ *         description: User or identity information not found.
  *       500:
- *         description: Internal Server Error.
+ *         description: Internal server error.
  */
 module.exports = async (req, res) => {
   const { wallet } = req.query
-  const timestamp = Date.now()
-  // TODO: get these fields from database
-  const clientId = 'CLIENT_ID_HERE'
-  const kycIssuedAt = Date.now() 
-  const fullName = 'John Doe'
-  const birthDate = '01.01.1995'
-  const identityNumber = '12345678910'
-  const nationality = 'TC'
-
-  const outputWitnessFilePath = `${appDir}/artifacts/CompleteIDVerification_${timestamp}.wtns`;
-  const outputProofFilePath = `${appDir}/artifacts/CompleteIDVerification_${timestamp}.proof.json`;
-  const outputPublicSignalsFilePath = `${appDir}/artifacts/CompleteIDVerification_${timestamp}.proof.ps.json`;
+  if(!wallet) {
+    return res.status(400).send('Wallet required')
+  }
   
-  // Prepare input for witness generation.
-  console.log("Preparing input for witness generation...");
-  const input = {
-    client_id_account_pair: hashSignals(clientId, wallet),
-    // TODO: Generate signature, We use client_id here temprarily
-    client_signature_issued_at_pair: hashSignals(clientId, kycIssuedAt),
-    full_name_birth_date_pair: hashSignals(fullName, birthDate),
-    identity_number_and_nationality_pair: hashSignals(identityNumber, nationality),
-  };
+  try {
+    const user = await User.findOne({ walletAddress: wallet })
+    if(!user) {
+      return res.status(404).send('User not found')
+    }
 
-  // Generate witness.
-  console.log("Generating witness...");
-  await prover.generateWitness(input, circuitWasmFilePath, outputWitnessFilePath);
+    const id = await IdentityVerification.findOne({ user })
+    if(!id) {
+      return res.status(404).send('Identity info not found')
+    }
 
-  // Generate proof and public signals.
-  console.log("Generating proof and public signals...");
-  const { proof, publicSignals } = await prover.prove(outputWitnessFilePath, zkeyFilePath, outputProofFilePath, outputPublicSignalsFilePath);
+    const timestamp = Date.now()
   
-  res.send({ proof, publicSignals })
+    const clientId = process.env.BLOCK_ID_CLIENT_ID
+    const kycIssuedAt = id.createdAt
+    const fullName = id.firstName + ' ' id.lastName
+    const birthDate = id.birthDate // Check if toString required
+    const identityNumber = id.identityNumber
+    const nationality = id.nationality
+  
+    const outputWitnessFilePath = `${appDir}/artifacts/CompleteIDVerification_${timestamp}.wtns`;
+    const outputProofFilePath = `${appDir}/artifacts/CompleteIDVerification_${timestamp}.proof.json`;
+    const outputPublicSignalsFilePath = `${appDir}/artifacts/CompleteIDVerification_${timestamp}.proof.ps.json`;
+    
+    // Prepare input for witness generation.
+    console.log("Preparing input for witness generation...");
+    const input = {
+      // TODO: Generate signature, We use client_id here temprarily
+      client_id_account_pair: hashSignals(clientId, wallet),
+      client_signature_issued_at_pair: hashSignals(clientId, kycIssuedAt),
+      full_name_birth_date_pair: hashSignals(fullName, birthDate),
+      identity_number_and_nationality_pair: hashSignals(identityNumber, nationality),
+    };
+  
+    // Generate witness.
+    console.log("Generating witness...");
+    await prover.generateWitness(input, circuitWasmFilePath, outputWitnessFilePath);
+  
+    // Generate proof and public signals.
+    console.log("Generating proof and public signals...");
+    const { proof, publicSignals } = await prover.prove(outputWitnessFilePath, zkeyFilePath, outputProofFilePath, outputPublicSignalsFilePath);
+    
+    return res.send({ proof, publicSignals })
+  } catch (err) {
+    console.error(err)
+    return res.status(500).send('Internal server error')
+  }
 }
