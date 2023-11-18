a97b9ebfe092f92aaa72ce30805650f824a16295
Samed Kahyaoglu <32146055+urtuba@users.noreply.github.com>
2023-11-18 21:32:04 +0300

    Add db name option to run multiple clients

--- client-server/utils/db.js
+++ client-server/utils/db.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose')
 
 const mongoUri = process.env.MONGO_URI || 'mongodb://localhost:27017/block-id'
+const dbName = process.env.MONGO_DB_NAME || 'block-id'
 
 const connectDb = () => {
-  mongoose.connect(mongoUri, { useNewUrlParser: true })
+  mongoose.connect(mongoUri + dbName, { useNewUrlParser: true })
   mongoose.connection.on('error', () => {
     throw new Error(`unable to connect to database: ${mongoUri}`)
   })
