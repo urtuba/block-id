34631c3bebb6a8604f29d618a14548cead309622
Samed Kahyaoglu <32146055+urtuba@users.noreply.github.com>
2023-11-19 05:18:45 +0300

    When orchestration fails do not process exit

--- block-id-server/utils.js
+++ block-id-server/utils.js
@@ -11,38 +11,49 @@
 // 3. An utility to get client information [clientId, clientName, clientUrl] from our contract
 // 4. A contract call to 'pingIdentityVerification(walletAddress, [proofs], demandingClient, result)' to only publish an event.
 
-import { getAuthorizedSources, getProofs, validateDataConsistency, validateProofs, validateDataConsistency, getGrantCodeFromSource } from "./helpers";
+import { getAuthorizedSources, getProofs, validateDataConsistency, validateProofs, validateDataConsistency, getGrantCodeFromSource, getClient } from "./helpers";
 
 
 orchestrateIdentitySync = async (walletAddress, demandingClientId) => {
   // TODO: this orchestration should be triggered when a syncRequest event is emitted. see BLOCKCHAIN CONNECTION REQUIREMENTS 2.
+  try {
+    const sources = await getAuthorizedSources(walletAddress)
+    console.log('DATA SOURCES FOUND:' + sources.length)
 
-  const sources = await getAuthorizedSources(walletAddress)
-  console.log('DATA SOURCES FOUND:' + sources.length)
+    const proofs = await getProofs(sources)
+    console.log('PROOFS FOUND:' + proofs.length)
+    
+    let proofsVerified
+    try {
+      proofsVerified = await validateProofs(proofs)
+    } catch (e) {
+      console.log('PROOFS VERIFICATION FAILED:' + e)
+      return
+    }
+    console.log('PROOFS VERIFIED:' + proofsVerified)
 
-  const proofs = await getProofs(sources)
-  console.log('PROOFS FOUND:' + proofs.length)
+    const isDataConsistent = validateDataConsistency(proofs)
+    console.log('DATA CONSISTENCY VERIFIED:' + isDataConsistent)
 
-  const proofsVerified = await validateProofs(proofs)
-  console.log('PROOFS VERIFIED:' + proofsVerified)
+    const result = proofsVerified && isDataConsistent
+    console.log('RESULT:' + result)
 
-  const isDataConsistent = validateDataConsistency(proofs)
-  console.log('DATA CONSISTENCY VERIFIED:' + isDataConsistent)
+    const sourceIndex = Math.floor(Math.random() * sources.length)
+    const sourceClient = sources[sourceIndex]
+    console.log('SOURCE CLIENT:' + JSON.stringify(sourceClient))
+    const grantCode = await getGrantCodeFromSource(sourceClient)
 
-  const result = proofsVerified && isDataConsistent
-  console.log('RESULT:' + result)
-
-  const sourceIndex = Math.floor(Math.random() * sources.length)
-  const sourceClient = sources[sourceIndex]
-  console.log('SOURCE CLIENT:' + JSON.stringify(sourceClient))
-  const grantCode = await getGrantCodeFromSource(sourceClient)
-
-  const { data, status } = await axios.post(demandingClientId + '/block-id/callback', { code: grantCode, wallet: walletAddress, source: sourceClient.clientUrl }, { params: { 'block-id-api-key': process.env.API_KEY } })
-  const success = status === 200
-  if(success) {
-    console.log('Identity data synced channel opened with ' + demandingClient.name)
-  } else {
-    console.log('Identity data sync failed with ' + demandingClient.name)
+    const demandingClient = await getClient(demandingClientId)
+    const { data, status } = await axios.post(demandingClientId + '/block-id/callback', { code: grantCode, wallet: walletAddress, source: sourceClient.clientUrl }, { params: { 'block-id-api-key': process.env.API_KEY } })
+    const success = status === 200
+    if(success) {
+      console.log('Identity data synced channel opened with ' + demandingClient.name)
+    } else {
+      console.log('Identity data sync failed with ' + demandingClient.name)
+    }
+  } catch (e) {
+    console.log('Identity data sync failed with client with id ' + demandingClientId)
+    console.error(e)
   }
 }
 
